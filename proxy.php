907470c4c37714baff5823da346d6f5cb3c2291a
pandii <affandianugrhh@gmail.com>
2025-06-01 18:41:12 +0700

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -11,7 +11,32 @@ if (!isset($_GET['url'])) {
 }
 
 $tiktokUrl = urlencode($_GET['url']);
-$apiUrl = "https://tikwm.com/api?url=$tiktokUrl";
+$apiUrl = "https://tikwm.com/api?url=$tiktokUrl&cache_bust=" . time(); 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
-$response = file_get_contents($apiUrl);
-echo $response;
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo json_encode([
+        "code" => -1,
+        "msg" => "cURL error: " . curl_error($ch)
+    ]);
+    curl_close($ch);
+    exit;
+}
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+if ($httpCode === 200) {
+    echo $response;
+} else {
+    echo json_encode([
+        "code" => $httpCode,
+        "msg" => "Failed to fetch from Tikwm API. HTTP Status: $httpCode"
+    ]);
+}
